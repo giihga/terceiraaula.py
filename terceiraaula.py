314701704685b2fe0d7625110f246d3fb8105606
giihga <102439784+giihga@users.noreply.github.com>
2022-04-16 16:00:07 -0300

    Add files via upload

---
+++ terceiraaula.py
@@ -0,0 +1,7 @@
+nome = input('Qual é seu nome ? ')
+idade = input('Quantos anos voce tem ?')
+formacao = input('Qual a sua formação academica?')
+ocupacao = input('Qual a sua Ocupação ? ')
+salario = input('Quanto você recebe de salario? ')
+
+print( nome,idade,formacao,oupacao,salario )
